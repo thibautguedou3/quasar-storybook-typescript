6824a47d46a3198761c848b9f20d345a78f1acd1
thibautguedou3 <thibaut@cood.fr>
2019-10-17 10:19:32 +0200

    :heavy_plus_sign: install add-on to see source files in storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,20 @@
+const path = require('path');
+
 module.exports = ({ config }) => {
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
+    test: /\.(scss|sass)$/,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../')
+  });
+  config.module.rules.push({
+    test: /\.stories\.ts?$/,
     use: [
       {
-        loader: 'ts-loader',
-        options: {
-          appendTsSuffixTo: [/\.vue$/]
-        }
+        loader: require.resolve('@storybook/source-loader'),
+        options: { parser: 'typescript' }
       }
-    ]
+    ],
+    enforce: 'pre'
   });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
